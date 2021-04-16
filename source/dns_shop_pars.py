b022a2b4efcf930c1ea0ef8d6359019774958b8a
skhe55 <flashback2019@mail.ru>
2021-04-16 22:28:17 +0700

    create methods for upload data to csv, xlsx form.

--- source/dns_shop_pars.py
+++ source/dns_shop_pars.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import csv
+import pandas as pd
+import xlsxwriter
 
 class DnsShopParser(object):
     def __init__(self, driver, req):
@@ -69,6 +72,9 @@ class DnsShopParser(object):
                 continue
 
         return index_list    
+    
+    def makeHyperLink(text:str, link:str):
+        return '=HYPERLINK("%s", "%s")'%(link, text)
 
     def print_average_price_prod(self, price_list:list, name_prod_list:list, link_prod_list:list, index_list:list):
         d = list()
@@ -97,16 +103,53 @@ class DnsShopParser(object):
 
         return d
 
+    def upload_to_csv_file(self, d:list, name_ctg:str):
+        try:
+            with open(name_ctg + "_data.csv", mode="w") as w_file:
+                names = ['Название товара', 'Цена товара', 'Ссылка на товар']
+                file_writer = csv.DictWriter(w_file, delimiter = ",", lineterminator="\r", fieldnames=names)
+                file_writer.writeheader()
+                file_writer.writerows(d)
+        except Exception:
+            return 'create csv file failed'
+
+    def upload_to_xlsx_file(self, d:list, name_ctg:str):
+        name_product = list()
+        price_product = list()
+        links_product = list()
+
+        for i in range(len(d)):
+            name_product.append(d[i].get('Название товара'))
+            price_product.append(d[i].get('Цена товара'))
+            links_product.append(d[i].get('Ссылка на товар'))
+
+        df = pd.DataFrame()
+        
+        df['Название товара'] = name_product
+        df['Цена товара'] = price_product
+        df['Ссылка на товар'] = links_product
+
+        writer = pd.ExcelWriter(name_ctg + '_product.xlsx', engine='xlsxwriter')
+        df.to_excel(writer, sheet_name='Page1', index=False)
+
+        writer.sheets['Page1'].set_column('A:A', 30)
+        writer.sheets['Page1'].set_column('B:B', 30)
+        writer.sheets['Page1'].set_column('C:C', 110)
+        writer.save()
+
+
 
 
 if __name__ == "__main__":
     list_get_requests = ["https://www.dns-shop.ru/catalog/17a89aab16404e77/videokarty/", "https://www.dns-shop.ru/catalog/17a899cd16404e77/processory/"]
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     ignored_exceptions = (NoSuchElementException,StaleElementReferenceException)
     driver = webdriver.Chrome(options=options, executable_path="chromedriver.exe")
     dns_pars = DnsShopParser(driver, list_get_requests[0])
     price, name, link = dns_pars.parse()
     index = dns_pars.average_price_pool(price, 3099, 5000)
-    print(dns_pars.print_average_price_prod(price, name, link, index))
+    d = dns_pars.print_average_price_prod(price, name, link, index)
+    dns_pars.upload_to_xlsx_file(d, "Videocard")
    
