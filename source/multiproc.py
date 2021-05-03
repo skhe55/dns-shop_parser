5e058f602b6bb7384f896694df07113d6b0ec648
skhe55 <flashback2019@mail.ru>
2021-05-04 01:44:33 +0700

    creating gui

--- source/multiproc.py
+++ source/multiproc.py
@@ -2,10 +2,47 @@ from multiprocessing import Process
 import time
 import os.path
 import pickle
-def save_data(d, num:str, _class_prod):
-    dir = os.path.abspath(os.curdir)
-    with open(dir[:-6] + 'data/' + 'data' + num + _class_prod + '.picle', 'wb') as f:
-        pickle.dump(d, f)
+
+sd = ['Alison', 'AMasn', 'Geny']
+def avg_price(d, max, min):
+    index_list = list()
+    for i in range(len(d)):
+        if d[i].get("Price") >= min and d[i].get("Price") <= max:
+            index_list.append(i)
+    return index_list        
+
+def most_manf(d, idx):
+    temp = list()
+    for i in range(len(idx)):
+        for j in range(len(sd)):
+            if sd[j] in d[i].get("Name"):
+                temp.append(sd[j])
+    return temp
 
 if __name__ == '__main__':
-    save_data('s', '1', 'Proc')
+    d = [
+        {
+            "Name":"Alison sd56546",
+            "Price": 4535,
+            "Link": "asdasd::"
+        },
+        {
+            "Name":"AMasn sdsdsd",
+            "Price": 1000,
+            "Link": "sdsddd::"
+        },
+        {
+            "Name":"Geny 435 34",
+            "Price": 3000,
+            "Link": "dfdf::"
+        },
+        {
+            "Name":"Alison xXXXXXX546",
+            "Price": 4225,
+            "Link": "asdasd::"
+        }
+    ]
+    idx = avg_price(d, 5000, 500)
+    temp = most_manf(d, idx)
+    print(idx)
+    print(temp.count('Alison'))
